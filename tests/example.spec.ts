1216c8d9c157a317656c648c0e822d19b45c1549
HirokiKuroda <kinaco.ao@gmail.com>
2022-10-17 18:26:02 +0900

    サンプルを作成

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,11 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("basic test", async ({ page }) => {
+  // titleがReact Appになっているかテスト
+  await page.goto("http://localhost:3000/");
+  await expect(page).toHaveTitle(/React App/);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-
-  // create a locator
-  const getStarted = page.getByText('Get Started');
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
-
-  // Click the get started link.
-  await getStarted.click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // pタグのテキストのテスト
+  const text = page.locator("p"); // locatorでタグを取得
+  await expect(text).toHaveText(/playwrigth test/);
 });
