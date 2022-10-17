ef3ca1efe0cb7f99696fd1d6c347e043c6090396
HirokiKuroda <kinaco.ao@gmail.com>
2022-10-17 20:09:34 +0900

    テストコードの追加

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>playwrigth test</p>
+        <p>Playwrigt</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,11 +1,16 @@
 import { test, expect } from "@playwright/test";
 
-test("basic test", async ({ page }) => {
+test("App.tsx", async ({ page }) => {
   // titleがReact Appになっているかテスト
   await page.goto("http://localhost:3000/");
   await expect(page).toHaveTitle(/React App/);
 
   // pタグのテキストのテスト
   const text = page.locator("p"); // locatorでタグを取得
-  await expect(text).toHaveText(/playwrigth test/);
+  await expect(text).toHaveText(/Playwrigt/);
+
+  //aタグのテスト
+  const learnReact = page.getByText("Learn React");
+  await expect(learnReact).toHaveAttribute("href", "https://reactjs.org");
+  await learnReact.click();
 });
